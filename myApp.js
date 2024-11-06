0ef931950d2d3870590e158c993dfd1b045c9daa
Sk Ismail <ismail.jnvb@gmail.com>
2024-11-06 17:50:27 +0530

    serve HTML

--- myApp.js
+++ myApp.js
@@ -1,10 +1,17 @@
 let express = require('express');
 let app = express();
-console.log("Hello World");
+// console.log("Hello World");
+
+// app.get('/',function(req,res){
+//     res.send("Hello World");
+// })
 
 app.get('/',function(req,res){
-    res.send("Hello World");
+    res.sendFile(__dirname + '/views/index.html');
 })
+
+
+app.use('/public',express.static(--__dirname + '/public'));
 
 
 
